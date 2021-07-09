755685535a07749f68500c7f7f5642804316eb01
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-08 20:37:29 -0400

    capture user input

---
+++ game.py
@@ -0,0 +1,14 @@
+# game.py 
+print("Rock, Paper, Scissors, Shoot!")
+
+# Ask for a user input
+
+x=input("Please choose one of either 'rock', 'paper' or 'scissors'")
+
+print(x)
+
+#Validate the user input
+
+#generate a computer choice
+
+# Determine the  winner
