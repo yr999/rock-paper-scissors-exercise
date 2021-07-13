ece32d0fe97ba707d26783d58f7e9621e3751697
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-13 19:10:47 -0400

    Determined the winner

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ x=input("Please choose one of either 'rock', 'paper' or 'scissors': ")
 
 
 print("User Input")
+
 print(x)
 
 #Validate the user input
@@ -35,4 +36,20 @@ print("Computer Chose")
 print(c)
 
 # Determine the  winner
+
+if (c == x):
+    print("Nobody won")
+elif(c == "rock") and (x == "scissors"):
+    print("Rock beats scissors and Computer won")
+elif(c == "scissors") and (x == "rock"):
+    print("Rock beats scissors and You won")
+elif(c == "paper") and (x == "rock"):
+    print("Paper beats rock and Computer won")
+elif(c == "rock") and (x == "paper"):
+    print("Paper beats rock and You won")
+elif(c == "scissors") and (x == "paper"):
+    print("Scissors beats paper and Computer won")
+elif(c == "paper") and (x == "scissors"):
+    print("Paper beats scissors and You won") 
+
 
