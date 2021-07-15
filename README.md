cfb962cf31ffeb64d0100db0a5f1c0f14f5859b7
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-14 21:35:14 -0400

    Cleaned up my codes and updated Readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rock-paper-scissors-exercise
+
+#activate virtual environmenta
+#how to install pip
+#conda instructions
+#python commands 
+#they can run
+#create local .env to have user name
+
+#example README file (you might have to customize this a little but its a good starting place for general organization):
+https://raw.githubusercontent.com/prof-rossetti/my-first-python-app/main/README.md
+
+#add code block ```
--- game.py
+++ game.py
@@ -10,64 +10,52 @@ load_dotenv() #> loads contents of the .env file into the script's environment
 
 user_name = os.getenv("USER_NAME")
 
+user_name=user_name.upper()
+
 print(user_name) # reads the variable from the environment
 
-# > "Hello World"
 
-exit()
-
-
-
-
-
-print("Rock, Paper, Scissors, Shoot!")
+print("ROCK, PAPER, SCISSORS, SHOOT!")
 
 # Ask for a user input
 
-x=input("Please choose one of either 'rock', 'paper' or 'scissors': ")
+x=input("PLEASE CHOOSE ONE OF EITHER 'ROCK','PAPER' OR 'SCISSORS': ")
 
-
-print("User Input")
-
-print(x)
+x=x.upper()
 
 #Validate the user input
 
-if  x == "rock" or (x == "paper") or (x == "scissors"):
-    print("Valid")
+if  x == "ROCK" or (x == "PAPER") or (x == "SCISSORS"):
+    print("VALID")
 else:
-    print("oops invalid, please try again")
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
     exit()
-print ("later messages") 
 
-
-
+print ("USER CHOSE: ", x) 
 
 #generate a computer choice
 
-valid_options = ["rock","paper","scissors"] #list to contain different variables
+valid_options = ["ROCK","PAPER","SCISSORS"] #list to contain different variables
 
 c = (random.choice(valid_options))
 
-print("Computer Chose")
-
-print(c)
+print("COMPUTER CHOSE:", c)
 
 # Determine the  winner
 
 if (c == x):
-    print("Nobody won")
-elif(c == "rock") and (x == "scissors"):
-    print("Rock beats scissors and Computer won")
-elif(c == "scissors") and (x == "rock"):
-    print("Rock beats scissors and You won")
-elif(c == "paper") and (x == "rock"):
-    print("Paper beats rock and Computer won")
-elif(c == "rock") and (x == "paper"):
-    print("Paper beats rock and You won")
-elif(c == "scissors") and (x == "paper"):
-    print("Scissors beats paper and Computer won")
-elif(c == "paper") and (x == "scissors"):
-    print("Paper beats scissors and You won") 
+    print("TIE")
+elif(c == "ROCK") and (x == "SCISSORS"):
+    print("YOU LOST")
+elif(c == "SCISSORS") and (x == "ROCK"):
+    print("YOU WON")
+elif(c == "PAPER") and (x == "ROCK"):
+    print("YOU LOST")
+elif(c == "ROCK") and (x == "PAPER"):
+    print("YOU WON")
+elif(c == "SCISSORS") and (x == "PAPER"):
+    print("YOU LOST")
+elif(c == "PAPER") and (x == "SCISSORS"):
+    print("YOU WON") 
 
 
